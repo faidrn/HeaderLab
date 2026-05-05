7c6eb4e43687123110afd9f697e7c9db45c79f95
Fredy Izquierdo <fredyizquierdo.dev@gmail.com>
2026-05-05 20:38:14 +0200

    Save changes

--- src/components/headers/GlassHeader/GlassHeader.jsx
+++ src/components/headers/GlassHeader/GlassHeader.jsx
@@ -13,7 +13,11 @@ const GlassHeader = () => {
         dark ? root.classList.add("dark") : root.classList.remove("dark");
     }, [dark]);
 
-    
+    useEffect(() => {
+        const onScroll = () => setScrolled(window.scrollY > 20);
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
 };
 
 export default GlassHeader;
